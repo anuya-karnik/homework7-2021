e1ea92cf90a809bceea6e451d8e77dbc25c8f25c
Anuya Karnik <aakarnik8@gmail.com>
2021-03-31 01:21:16 -0400

    play pause complete

--- js/video.js
+++ js/video.js
@@ -1,11 +1,30 @@
-var video;
+var video = document.querySelector("#player1");
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
 	console.log("Good job opening the window")
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function () {
+	console.log("Play Video");
+	video.play();
+});
+
+document.querySelector("#pause").addEventListener("click", function () {
+	console.log("Pause Video");
+	video.pause();
+});
+
+
+
+document.querySelector("#vintage").addEventListener("click", function () {
+	console.log("Change CSS");
+	video.classList.add("oldSchool");	
+});
+
+document.querySelector("#orig").addEventListener("click", function () {
+	console.log("Change CSS");
+	video.classList.remove("oldSchool");	
+});
+
 
