6042b83ca535850386b1a4ea70b62a29cdba23ca
Anuya Karnik <aakarnik8@gmail.com>
2021-03-31 03:04:32 -0400

    completed hw

--- js/video.js
+++ js/video.js
@@ -8,6 +8,7 @@ window.addEventListener("load", function () {
 document.querySelector("#play").addEventListener("click", function () {
 	console.log("Play Video");
 	video.play();
+	document.querySelector('#volume').innerHTML = (video.volume * 100) + "%";
 });
 
 document.querySelector("#pause").addEventListener("click", function () {
@@ -15,6 +16,47 @@ document.querySelector("#pause").addEventListener("click", function () {
 	video.pause();
 });
 
+document.querySelector("#slower").addEventListener("click", function () {
+	console.log("Slow Down Video");
+	video.playbackRate *= .95;
+	console.log("New speed is " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function () {
+	console.log("Speed Up Video");
+	video.playbackRate /= .95;
+	console.log("New speed is " + video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function () {
+	console.log("Skip Ahead 15 Seconds");
+	video.currentTime += 15;
+	if(video.currentTime >= video.duration) {
+		video.currentTime = 0;
+	}
+	console.log("Skipped ahead to " + video.currentTime);
+});
+
+
+document.querySelector("#slider").addEventListener("click", function () {
+	console.log("Changing Volume");
+	console.log(this.value);
+	video.volume = (this.value) / 100;
+	document.querySelector('#volume').innerHTML = (video.volume * 100) + "%";
+});
+
+document.querySelector("#mute").addEventListener("click", function () {
+	if(video.muted) {
+		console.log("Unmute Video");
+		video.muted = false;
+		document.querySelector("#mute").innerHTML = "Mute"
+	}
+	else {
+		console.log("Mute Video")
+		video.muted = true;
+		document.querySelector("#mute").innerHTML = "Unmute"
+	}
+});
 
 
 document.querySelector("#vintage").addEventListener("click", function () {
